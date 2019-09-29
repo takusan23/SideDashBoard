427a4ff541ca268e8abdb85d5ff3e28950b1b8cc
takusan23 <takumu1200@gmail.com>
2019-09-30 01:53:07 +0900

    修正

--- src/main.js
+++ src/main.js
@@ -56,15 +56,6 @@ function createWindow() {
     // タスクトレイが左クリックされた場合、アプリのウィンドウをアクティブに
     tray.on("click", function () {
         mainWindow.focus();
-
-        var spawn = require('child_process').spawn;
-
-        temp = spawn('cat', ['/sys/class/thermal/thermal_zone0/temp']);
-
-        temp.stdout.on('data', function (data) {
-            console.log('Result: ' + data / 1000 + ' degrees Celcius');
-        });
-
     });
 }
 
